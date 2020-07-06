de138ebd9b6e1a6d8f93e391fb73b9d7b8cd0338
LeeJaeBae <nea4182@gmail.com>
2020-07-06 15:35:36 +0900

    body,cookie - parser

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on http://localhost:${PORT}`);
+
+app.use(morgan("dev")); // logging
+app.use(helmet());
 
 app.get("/", (req, res) => {
 	res.send("home");
