3d597eb530d0e74e7485dffd53223bde35b5bd6c
LeeJaeBae <nea4182@gmail.com>
2020-07-06 17:08:06 +0900

    divide routes, controllers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos);
-videoRouter.get(routes.upload);
-videoRouter.get(routes.videoDetail);
-videoRouter.get(routes.editVideo);
-videoRouter.get(routes.deleteVideo);
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
