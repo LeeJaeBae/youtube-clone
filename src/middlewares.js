1b448a6f1f7dd39fcac69c905ebcaf67f9fc2b90
LeeJaeBae <nea4182@gmail.com>
2020-07-13 15:32:18 +0900

    View:: search

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,7 @@
-import routes from "./src/routers/routes";
+import routes from "./routers/routes";
 
 export const localsMiddleware = (req, res, next) => {
-	res.locals.siteName = "YOUTEBE";
+	res.locals.siteName = "YOUTUBE";
 	res.locals.routes = routes;
 	next();
 };
