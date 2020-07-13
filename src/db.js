617a1edb221662b4b55dbca0588bd859b546c139
LeeJaeBae <nea4182@gmail.com>
2020-07-13 11:08:17 +0900

    View:: mixin -> videoBlock

---
+++ src/db.js
@@ -0,0 +1,55 @@
+/** dummy data */
+export const videos = [
+	{
+		id: 123123,
+		title: "Video test1",
+		description: "discription",
+		views: 24,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			user: "wony",
+			email: "test@naver.com",
+		},
+	},
+	{
+		id: 123124,
+		title: "Video test2",
+		description: "discription",
+		views: 24,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			user: "wony",
+			email: "test@naver.com",
+		},
+	},
+	{
+		id: 123125,
+		title: "Video test3",
+		description: "discription",
+		views: 24,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			user: "wony",
+			email: "test@naver.com",
+		},
+	},
+	{
+		id: 123126,
+		title: "Video test4",
+		description: "discription",
+		views: 24,
+		videoFile:
+			"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+		creator: {
+			id: 121212,
+			user: "wony",
+			email: "test@naver.com",
+		},
+	},
+];
